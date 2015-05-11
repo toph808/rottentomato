33badffe129ca2486c2f8f8d186b212ab8efb8ae
toph <kris@shopular.com>
2015-05-10 23:34:51 -0700

    Progress spinner, pull to refresh, error msg with icon

--- rottentomato/rottentomato/MoviesViewController.swift
+++ rottentomato/rottentomato/MoviesViewController.swift
@@ -11,24 +11,21 @@ import UIKit
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var errorImageView: UIImageView!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var errorIconImageView: UIImageView!
     
     var movies: [NSDictionary]?
+    var refreshControl:UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/e41513a57049e21bc6cf/raw/b490e79be2d21818f28614ec933d5d8f467f0a66/gistfile1.json")!
-        let req = NSMutableURLRequest(URL: url)
-        NSURLConnection.sendAsynchronousRequest(req, queue: NSOperationQueue.mainQueue(), completionHandler: {
-            (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
-            var errorValue: NSError? = nil
-            let resp = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary
-            
-            if let resp = resp {
-                self.movies = resp["movies"] as? [NSDictionary]
-                self.tableView.reloadData()
-            }
-        })
+        
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        self.tableView.insertSubview(self.refreshControl, atIndex: 0)
+    
+        queryRottenTomatoes()
         
         tableView.dataSource = self
         tableView.delegate = self
@@ -37,6 +34,43 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func queryRottenTomatoes() {
+        SVProgressHUD.show()
+        
+        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/e41513a57049e21bc6cf/raw/b490e79be2d21818f28614ec933d5d8f467f0a66/gistfile1.json")!
+        let req = NSMutableURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(req, queue: NSOperationQueue.mainQueue(), completionHandler: {
+            (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            let resp = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary
+            
+            if let resp = resp {
+                self.movies = resp["movies"] as? [NSDictionary]
+                self.tableView.reloadData()
+                
+                SVProgressHUD.dismiss()
+            } else {
+                self.showErrorView()
+            }
+        })
+    }
+    
+    func onRefresh() {
+        queryRottenTomatoes()
+        self.refreshControl.endRefreshing()
+    }
+    
+    func showErrorView() {
+        self.errorImageView.hidden = false
+        self.errorLabel.hidden = false
+        self.errorIconImageView.hidden = false
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (Int64)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), {
+            self.errorImageView.hidden = true
+            self.errorLabel.hidden = true
+            self.errorIconImageView.hidden = true
+        })
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
